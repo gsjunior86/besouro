2e011cb8c378957460dd977699f252c387229f60
Geraldo <gsjunior86@gmail.com>
2020-12-05 06:08:04 +0100

    added comment

--- src/main/scala/br/gsj/besouro/BesouroStarter.scala
+++ src/main/scala/br/gsj/besouro/BesouroStarter.scala
@@ -2,10 +2,11 @@ package br.gsj.besouro
 
 import br.gsj.server.besouro.BesouroServer
 
+
 object BesouroStarter {
   
   def main(args: Array[String]): Unit = {
-    
+	//start the server    
     val server = new BesouroServer(2222)
     server.run()
     
